8cebfe5a0a02ae284af4b0f7c9bdf934efc85b58
skasero <spencerkkr@gmail.com>
2019-09-22 18:10:21 -0700

    A better DT_train_binary maybe?

--- project1/decision_trees.py
+++ project1/decision_trees.py
@@ -7,8 +7,10 @@ def DT_train_binary(X,Y,max_depth):
 	DT = DT_train_binary_helper(X,Y,max_depth,h,0)
 	print("root: " + str(DT.key))
 	printTree(DT,1)
+	return DT
 
 def DT_train_binary_helper(X,Y,max_depth,h,currentDepth = 0,featuresUsed = list()):
+	print("\n depth: ", currentDepth)
 	# Found that there are no more Samples to check in X
 	if(max_depth == -1 and len(X) <= 1):
 		return None
@@ -22,7 +24,11 @@ def DT_train_binary_helper(X,Y,max_depth,h,currentDepth = 0,featuresUsed = list(
 			rightHeuristic = calcHeuristic(X,Y,True,i)
 			ig = calcIG(h,leftHeuristic,rightHeuristic,X,i)
 			n = tree.Node(i,leftHeuristic,rightHeuristic,ig)
+			#print(str(leftHeuristic) + " " + str(rightHeuristic))
+			print(ig)
 			nodeArray.append(n)
+	if(len(nodeArray) == 0):
+		return None
 
 	highestIG = 0
 	index = 0
@@ -33,24 +39,33 @@ def DT_train_binary_helper(X,Y,max_depth,h,currentDepth = 0,featuresUsed = list(
 			featureNumber = node.key
 			index = nodeArray.index(node)
 	featuresUsed.append(featureNumber)
-	remove = 1
-	# if there are more yeses than nos, else keep remove as 1 for opposite
-	if(nodeArray[index].leftHeuristic < nodeArray[index].rightHeuristic):
-		remove = 0
 
+	node = nodeArray[index]
+	if(node.leftHeuristic == 0 and node.rightHeuristic != 0):
+		print("right")
+		X,Y = removeFeature(0,X,Y,featureNumber)
+		node.add_leaves(None,DT_train_binary_helper(X,Y,max_depth,node.rightHeuristic,currentDepth+1,featuresUsed))
+	elif(node.leftHeuristic != 0 and node.rightHeuristic == 0):
+		print("left")
+		X,Y = removeFeature(1,X,Y,featureNumber)
+		node.add_leaves(DT_train_binary_helper(X,Y,max_depth,node.leftHeuristic,currentDepth+1,featuresUsed),None)
+	elif(node.leftHeuristic == 0 and node.rightHeuristic == 0):
+		print("empty")
+		node.add_leaves(None,None)
+	elif(node.leftHeuristic != 0 and node.rightHeuristic != 0):
+		print("2 splits")
+		node.add_leaves(DT_train_binary_helper(X,Y,max_depth,node.leftHeuristic,currentDepth+1,featuresUsed),DT_train_binary_helper(X,Y,max_depth,node.rightHeuristic,currentDepth+1,featuresUsed))
+	return node
+
+def removeFeature(binary,X,Y,featureNumber):
 	removeCount = 0
 	for i in range(0,len(X)):
-		if(X[i-removeCount][featureNumber] == remove):
+		if(X[i-removeCount][featureNumber] == binary):
 			X = np.delete(X,i-removeCount,0)
 			Y = np.delete(Y,i-removeCount,0)
 			removeCount += 1
+	return X,Y
 
-	node = nodeArray[index]
-	if(nodeArray[index].leftHeuristic < nodeArray[index].rightHeuristic):
-		node.add_leaves(None,DT_train_binary_helper(X,Y,max_depth,node.rightHeuristic,currentDepth+1,featuresUsed))
-	else:
-		node.add_leaves(DT_train_binary_helper(X,Y,max_depth,node.leftHeuristic,currentDepth+1,featuresUsed),None)
-	return node
 
 # X = Feature array
 # Y = Label array
@@ -75,7 +90,9 @@ def calcHeuristic(X,Y,boolean = None,index = 0):
 			# the label is 1(yes), aka counting the yeses
 			if Y[i][0] == 1:
 				count += 1
-		
+	
+	if(total == 0):
+		return 0
 	percent = count/total # this is the percent of yeses
 	# This checks if python will run into a RuntimeWarning b/c log(0)
 	if(percent != 0 and (1-percent) != 0):
@@ -107,11 +124,48 @@ def printTree(node,depth):
 	printTree(node.leaves[0],depth+1)
 	printTree(node.leaves[1],depth+1)
 
+def DT_test_binary(X,Y,DT):
+	pass
 
 if __name__ == "__main__":
+	#first example in project
 	X = np.array([[0,1,0,1],[1,1,1,1],[0,0,0,1]])
 	Y = np.array([[1],[1],[0]])
+
+	#netflix
 	test1 = np.array([[1,1,0,0],[1,1,1,1],[1,1,1,1],[0,0,0,1],[0,0,1,1],[0,0,1,0],[0,0,0,0],[1,0,1,0],[1,1,1,0],[0,0,1,1]])
 	test1label = np.array([[0],[1],[1],[0],[0],[1],[0],[0],[1],[0]])
+	# all 8
+	test2 = np.array([[1,1,1,0,1,1,0],[0,0,1,1,0,1,1],[0,1,0,0,1,0,0],[1,1,0,1,0,0,1],[1,0,1,0,1,1,1],[1,1,0,1,1,0,1],[1,1,0,0,1,1,0],[0,0,0,1,0,1,1]])
+	test2label = np.array([[1],[1],[0],[1],[0],[1],[1],[0]])
+	#first 5
+	test3 = np.array([[1,1,1,0,1,1,0],[0,0,1,1,0,1,1],[0,1,0,0,1,0,0],[1,1,0,1,0,0,1],[1,0,1,0,1,1,1]])
+	test3label = np.array([[1],[1],[0],[1],[0]])
+	#last 5
+	test4 = np.array([[1,1,0,1,0,0,1],[1,0,1,0,1,1,1],[1,1,0,1,1,0,1],[1,1,0,0,1,1,0],[0,0,0,1,0,1,1]])
+	test4label = np.array([[1],[0],[1],[1],[0]])
+	#middle 2-6
+	test5 = np.array([[0,0,1,1,0,1,1],[0,1,0,0,1,0,0],[1,1,0,1,0,0,1],[1,0,1,0,1,1,1],[1,1,0,1,1,0,1]])
+	test5label = np.array([[1],[0],[1],[0],[1]])
+	#middle 3-7
+	test6 = np.array([[0,1,0,0,1,0,0],[1,1,0,1,0,0,1],[1,0,1,0,1,1,1],[1,1,0,1,1,0,1],[1,1,0,0,1,1,0]])
+	test6label = np.array([[0],[1],[0],[1],[1]])
 	max_depth = -1
-	DT_train_binary(test1,test1label,max_depth)
+	#custom
+	test7 = np.array([[1,1,0],[1,1,0],[1,1,1],[1,0,0],[1,0,1],[0,0,1],[0,1,1],[0,1,0],[0,0,0]])
+	test7label = np.array([[1],[1],[1],[0],[0],[1],[1],[0],[0]])
+
+	#Traing set 1
+	test8 = np.array([[0,1],[0,0],[1,0],[0,0],[1,1]])
+	test8label = np.array([[1],[0],[0],[0],[1]])
+
+	#DT_train_binary(test1,test1label,max_depth)
+	#DT_train_binary(test3,test3label,5)
+	#DT_train_binary(test4,test4label,5)
+	#DT_train_binary(test5,test5label,5)
+	#DT_train_binary(test6,test6label,5)
+	DT_train_binary(test7,test7label,-1)
+	#DT_train_binary(test8,test8label,-1)
+
+
+
